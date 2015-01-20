72a3f22d127b7494801eadca479ed6e66c1c9422
Weyland Joyner <weylandjoyner4@gmail.com>
2015-01-19 22:23:11 -0500

    fixed bonsai

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,1 +1,5 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
+if Rails.env == "development"
+  Elasticsearch::Model.client = Elasticsearch::Client.new
+else
+  Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
+end
