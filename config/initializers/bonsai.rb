0f94a708f215f6179bedebd43557f7699a65d27f
Weyland Joyner <weylandjoyner4@gmail.com>
2015-01-21 11:46:09 -0500

    fixed typo in bonsai

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
   Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
-else Rails.env.development?
-Elasticsearch::Model.client = Elasticsearch::Client.new
+else
+  Elasticsearch::Model.client = Elasticsearch::Client.new
 end
