b49c32ca629f67deead7c5b6b2cd981d8b32891e
Weyland Joyner <weylandjoyner4@gmail.com>
2015-01-21 14:16:04 -0500

    disabled elasticsearch again

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,11 @@
-require 'elasticsearch/model'
-require 'elasticsearch/rails'
+#require 'elasticsearch/model'
+#require 'elasticsearch/rails'
 
 class Question < ActiveRecord::Base
   has_many :answers
   has_many :posts, through: :answers, inverse_of: :question
 
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
+  #include Elasticsearch::Model
+  #include Elasticsearch::Model::Callbacks
 end
-Question.import
+#Question.import
--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,5 +1,5 @@
-if Rails.env.production?
-  Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
-else
-  Elasticsearch::Model.client = Elasticsearch::Client.new
-end
+#if Rails.env.production?
+#  Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
+#else
+#  Elasticsearch::Model.client = Elasticsearch::Client.new
+#end
