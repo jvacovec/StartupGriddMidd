3603535acf7df6ccecf78eab87202e2b66a19b98
Weyland Joyner <weylandjoyner4@gmail.com>
2015-01-22 14:18:06 -0500

    fixed bonsai init

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,9 +1,5 @@
-require 'elasticsearch/model'
-
-if Ruby.env.production?
-  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], logs: true})
-  BONSAI_INDEX_NAME = ENV['BONSAI_URL'][/[^\/]+$/]
+if Rails.env.production?
+  Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
 else
-  app_name = Rails.application.class.parent_name.underscore.dasherize
-  BONSAI_INDEX_NAME = "#{app_name}-#{Rails.env}"
+  Elasticsearch::Model.client = Elasticsearch::Client.new
 end
