007a9583f27fc1f5d331aa7e8c0c3fdad85c0e63
Will Potter <willstonepotter@gmail.com>
2015-01-23 13:54:56 -0500

    fixing posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@
 class Post < ActiveRecord::Base
   belongs_to :user, inverse_of: :posts
   belongs_to :author, inverse_of: :posts
-  has_many :answers, inverse_of: :post
-  has_many :questions, through: :answers, inverse_of: :post
+  has_many :answers, inverse_of: :posts
+  has_many :questions, through: :answers, inverse_of: :posts
   has_many :taggings
   has_many :tags, through: :taggings
 
