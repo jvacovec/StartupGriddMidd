1e3037fb0c198c4edfed97e11dcfaca5c04e4f39
Will Potter <willstonepotter@gmail.com>
2015-01-26 15:57:02 -0500

    fixing search controller to include authors

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,10 @@
 class SearchController < ApplicationController
-	def search
-	  if not params[:q].nil?
-	    posts = Post.search(params[:q]).map {|p| p['_source']}
-	   else 
-	   	posts = []
-	  end
-	  render json: posts, include: [:author, :user, :tags, :answers] 
-	end
+  def search
+    if not params[:q].nil?
+      @posts = Post.search(params[:q]).map {|p| Post.find(p['_source']['id'])}
+     else 
+      @posts = []
+    end
+    render json: @posts, include: [:author]
+  end
 end
