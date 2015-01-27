21c3ef447c5695dc7d3c31a3e57080ff693748aa
Teddy Knox <teddyknox@gmail.com>
2015-01-27 11:45:40 -0500

    Delete a bunch of actions and controllers.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,13 +1,5 @@
 class TagsController < ApplicationController
 
-  # GET /tags/1
-  # GET /tags/1.json
-  def show
-    logger.info params[:id]
-    @tags = Tag.includes(children: [:children]).find(params[:id])
-    render json: @tags.map { |t| t.to_tree }
-  end
-
   def topics
     render_tree = params[:tree] == "false" ? false : true
     @topics = Tag.where(:parent_id => nil, :custom => false).order(:name)
@@ -22,44 +14,4 @@ class TagsController < ApplicationController
     @posts = Tag.find(params[:id]).posts
     render json: @posts, :joins => [:author, :user, :tags, {:questions => {:include => :answers}}]
   end
-
-
-  # POST /tags
-  # POST /tags.json
-  def create
-    @tag = Tag.new(tag_params)
-
-    if @tag.save
-      render json: @tag, status: :created, location: @tag
-    else
-      render json: @tag.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /tags/1
-  # PATCH/PUT /tags/1.json
-  def update
-    @tag = Tag.find(params[:id])
-
-    if @tag.update(tag_params)
-      head :no_content
-    else
-      render json: @tag.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /tags/1
-  # DELETE /tags/1.json
-  def destroy
-    @tag = Tag.find(params[:id])
-    @tag.destroy
-
-    head :no_content
-  end
-
-  private
-    
-    def tag_params
-      params[:tag]
-    end
 end
