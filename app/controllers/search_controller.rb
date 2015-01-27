6822c5693da7d171344e8201010905ff964735ed
Weyland Joyner <weylandjoyner4@gmail.com>
2015-01-27 16:37:53 -0500

    fixing indentation in search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,10 @@
 class SearchController < ApplicationController
-	def search
-	  if not params[:q].nil?
-	    posts = Post.search(params[:q]).map{ |p| p["_source"]}
-	  else 
-	   	posts = []
-	  end
-	  render json: posts
-	end
+  def search
+    if not params[:q].nil?
+      posts = Post.search(params[:q]).map{ |p| p["_source"]}
+    else 
+       posts = []
+    end
+    render json: posts
+  end
 end
