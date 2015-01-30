723db3297df53ec3a6454b212833de9de3a33506
Teddy Knox <teddyknox@gmail.com>
2015-01-30 02:13:40 -0500

    Trying another way.

--- config/application.rb
+++ config/application.rb
@@ -23,10 +23,10 @@ module SUG
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.use Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get], :expose => ["Link"]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options], :expose => ['Link']
       end
     end
   end
